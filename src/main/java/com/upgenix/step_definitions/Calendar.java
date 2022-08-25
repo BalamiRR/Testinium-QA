df8315a77a398321d6ab842ef4f4ac3bf4bfeb73
unknown <furkankara.9528@gmail.com>
2022-08-25 13:27:49 +0200

    Calendar page and scenario were created

--- src/main/java/com/upgenix/step_definitions/Calendar.java
+++ src/main/java/com/upgenix/step_definitions/Calendar.java
@@ -1,4 +1,50 @@
 package com.upgenix.step_definitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Calendar {
+
+    @When("User clicks on day button")
+    public void user_clicks_on_day_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User clicks on week button")
+    public void user_clicks_on_week_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User clicks on month button")
+    public void user_clicks_on_month_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should see the last stage of calendar view")
+    public void user_should_see_the_last_stage_of_calendar_view() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User is on the calendar dashboard")
+    public void user_is_on_the_calendar_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User clicks on Calender Module")
+    public void user_clicks_on_calender_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User is on the Calender Module")
+    public void user_is_on_the_calender_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }

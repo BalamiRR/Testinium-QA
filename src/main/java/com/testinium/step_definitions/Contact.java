ef1e723a107813755fd25c8198607975d63c32ed
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2022-09-03 11:23:52 +0200

    Contact's step definition is created and new methods are added from feature file

---
+++ src/main/java/com/testinium/step_definitions/Contact.java
@@ -0,0 +1,70 @@
+package com.testinium.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Contact {
+
+    @When("User is at Contact dashboard")
+    public void user_is_at_contact_dashboard() {
+    }
+
+    @When("User clicks the create button")
+    public void user_clicks_the_create_button() {
+    }
+
+    @When("User enters name {string}")
+    public void user_enters_name(String string) {
+
+    }
+
+    @When("User enters {string}")
+    public void user_enters(String string) {
+
+    }
+
+    @When("User enters {string} and {string}")
+    public void user_enters_and(String string, String string2) {
+
+    }
+
+    @Then("User sees the updated contact details at dashboard")
+    public void user_sees_the_updated_contact_details_at_dashboard() {
+
+    }
+
+    @When("User clicks list section and choose the profile")
+    public void user_clicks_list_section_and_choose_the_profile() {
+
+    }
+
+    @When("User clicks Action to choose delete button")
+    public void user_clicks_action_to_choose_delete_button() {
+
+    }
+
+    @When("User clicks and goes directly to the profile")
+    public void user_clicks_and_goes_directly_to_the_profile() {
+
+    }
+
+    @Then("User sees deleted profile")
+    public void user_sees_deleted_profile() {
+
+    }
+
+    @When("User selects the profile")
+    public void user_selects_the_profile() {
+
+    }
+
+    @When("User clicks the print button and then select due payments")
+    public void user_clicks_the_print_button_and_then_select_due_payments() {
+
+    }
+
+    @Then("User can see the dowloaded file")
+    public void user_can_see_the_dowloaded_file() {
+        
+    }
+}
